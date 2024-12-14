ffd3bd3f85b76a8204108aa2788ab66f7991d0ba
Dryjok-Pirojok <nikitasitnikov333334456@gmail.com>
2024-12-14 14:59:32 +0300

    Небольшая оптимизация

--- data/base/game_loop.py
+++ data/base/game_loop.py
@@ -34,50 +34,56 @@ def Loop():
     running = True
     cols_x = 50
     cols_y = 50
-    cam_pos_x = -700
-    cam_pos_y = -0
+    cam_pos_x = 0
+    cam_pos_y = 0
     fps = 144
     k = 0
     base_x = 0
     base_y = 0
-    left_button = False
+    right_button = False
     while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == pygame.BUTTON_LEFT:
-                    left_button = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == pygame.BUTTON_RIGHT:
+                    right_button = True
                     base_x = event.pos[0]
                     base_y = event.pos[1]
 
-            elif event.type == pygame.MOUSEMOTION:
-                if left_button:
+            if event.type == pygame.MOUSEMOTION:
+                if right_button:
                     x = event.pos[0]
                     y = event.pos[1]
-                    cam_pos_x = cam_pos_x + (base_x - x) // 10
-                    cam_pos_y = cam_pos_y + (base_y - y) // 10
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if event.button == pygame.BUTTON_LEFT:
-                    left_button = False
-        screen.fill("black")
-        thread_ground = threading.Thread(target=draw_ground_grid, args=(cols_x, cols_y, grid, weight,
-                                                                        cam_pos_x, cam_pos_y))
-        thread_wall = threading.Thread(target=draw_walls_grid, args=(cols_x, cols_y, grid, weight,
-                                                                              cam_pos_x, cam_pos_y))
-        thread_ground.start()
-        thread_wall.start()
+                    cam_pos_x = cam_pos_x + (base_x - x)
+                    cam_pos_y = cam_pos_y + (base_y - y)
+                    base_x = event.pos[0]
+                    base_y = event.pos[1]
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == pygame.BUTTON_RIGHT:
+                    right_button = False
+        if k % 100 == 0 or right_button:
+            screen.fill("black")
+            thread_ground = threading.Thread(target=draw_ground_grid, args=(cols_x, cols_y, grid, weight,
+                                                                            cam_pos_x, cam_pos_y))
+            thread_wall = threading.Thread(target=draw_walls_grid, args=(cols_x, cols_y, grid, weight,
+                                                                                  cam_pos_x, cam_pos_y))
+            thread_ground.start()
+            thread_ground.join()
+            thread_wall.start()
 
-        thread_wall.join()
-        thread_ground.join()
-        k = 0
-
+            thread_wall.join()
+            k = 0
         k += 1
         pygame.display.flip()
+
         clock.tick(fps)
 
+
+
         print(clock.get_fps())
+        print(k)
     pygame.quit()
 
 if __name__ == "__main__":
